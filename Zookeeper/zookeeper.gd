9dbe2dd852156e1342497b12d2c5aea0fa68c74c
Oli Klein <zwietabak@gmail.com>
2024-01-27 23:00:10 +0100

    optimized zookeeper and visitor

--- Zookeeper/zookeeper.gd
+++ Zookeeper/zookeeper.gd
@@ -4,11 +4,14 @@ class_name Zookeeper
 
 const SPEED = 4.0
 const ATTACK_TIMEOUT: float = 2.0
+const DASH_COOLDOWN: float = 10.0
+const DASH_POWER: float = 4.0
 
 @onready var navigation_agent = $Navigation_Agent
 @onready var state_machine = $ZookeeperStateMachine
 
 var attack_on_cooldown: bool = false
+var dash_on_cooldown: bool = false
 var dead: bool
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,7 +22,8 @@ func _ready() -> void:
 
 func receive_damage(damage_direction: Vector3) -> void:
 	if !dead:
-		state_machine.transition_to("Death", {"damage_direction": damage_direction})
+		#state_machine.transition_to("Death", {"damage_direction": damage_direction})
+		pass
 
 func _physics_process(delta) -> void:
 	pass
@@ -30,7 +34,7 @@ func update_target_location(target_location: Vector3) -> void:
 
 
 func _on_navigation_agent_target_reached():
-	if not attack_on_cooldown and not dead:
+	if !attack_on_cooldown and !dead:
 		state_machine.transition_to("Attack")
 		start_attack_cooldown()
 
@@ -44,10 +48,30 @@ func start_attack_cooldown() -> void:
 	timer.timeout.connect(_attack_cooldown_timer_done.bind(timer))
 	timer.start()
 	attack_on_cooldown = true
-	print("attack cooldown started")
 
 
 func _attack_cooldown_timer_done(timer: Timer) -> void:
 	attack_on_cooldown = false
 	timer.queue_free()
-	print("attack cooldown done")
+
+
+func _on_dash_trigger_entered(body):
+	if body.name == "Player" and !dash_on_cooldown:
+		print("trigger on enemy")
+		state_machine.transition_to("Charge_Dash")
+
+
+func start_dash_cooldown() -> void:
+	var timer: Timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+	timer.autostart = false
+	timer.wait_time = DASH_COOLDOWN
+	timer.timeout.connect(_dash_cooldown_timer_done.bind(timer))
+	timer.start()
+	dash_on_cooldown = true
+
+
+func _dash_cooldown_timer_done(timer: Timer) -> void:
+	dash_on_cooldown = false
+	timer.queue_free()
