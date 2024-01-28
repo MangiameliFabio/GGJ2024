e0cb8319334511ac5d1a539cc5b9bfafb4fb9616
Robin <robin@hohnsbeen.de>
2024-01-28 11:35:26 +0100

    Added random material texture script + set material texture for zoo keeper

--- Zookeeper/RandomShirt.gd
+++ Zookeeper/RandomShirt.gd
@@ -7,14 +7,19 @@ var MeshNode : MeshInstance3D
 @export
 var ShirtMaterial : ShaderMaterial
 
+@export
+var MaterialIndex = 1
+
+@export
+var StartColor : Color = Color(0.8, 0.4, 0.8)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var shirtcolor : Color = Color(0.8, 0.4, 0.8)
-	shirtcolor.h = randf()
+	StartColor.h = randf()
 	
-	ShirtMaterial.set_shader_parameter("Color", shirtcolor)
-	MeshNode.set_surface_override_material(1, ShirtMaterial)
+	ShirtMaterial.set_shader_parameter("Color", StartColor)
+	MeshNode.set_surface_override_material(MaterialIndex, ShirtMaterial)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
